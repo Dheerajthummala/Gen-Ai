4f5b179f4e4ceb5598f377a1bcc68640098d5b4d
willsee-ux <willseeguys@gmail.com>
2026-01-07 21:38:25 +0530

    starting

---
+++ Day_1/day1.py
@@ -0,0 +1,23 @@
+def addition(a,b):
+    return a+b
+def subraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b  
+def division(a,b):
+    return a/b   
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+selection = input("select(+,-,*,/): ")
+while selection not in ["+","-","*","/"]:
+    selection = input("select(+,-,*,/): ")
+result = 0
+if selection == "+":
+    result = addition(x, y)
+elif selection == "-":
+    result = subraction(x, y)
+elif selection == "*":
+    result = multiplication(x, y)
+elif selection == "/":
+    result = division(x, y)
+print("Result:", result)
